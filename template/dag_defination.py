2d9500ff633f7eab7b9c16c6cd9edb418c7e5279
何梦芸 <campanulamediuml@gmail.com>
2025-10-03 12:20:27 +0800

    调整结构

--- template/dag_defination.py
+++ template/dag_defination.py
@@ -1,3 +1,18 @@
 # !/usr/bin/env python
 # -*-coding:utf-8 -*-
 # Author     ：Campanula 梦芸 何
+from typing import TypedDict, Dict
+
+
+class PluginParams(TypedDict):
+    common_param: Dict
+    parameter: Dict[str, Dict]
+
+
+class TypeDAGNode(TypedDict):
+    id: str
+    node_type: str
+    module_name: str
+    operator_type: str
+    operator_params: Dict
+    plugin_params: PluginParams
